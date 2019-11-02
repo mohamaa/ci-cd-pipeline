ee65a62e7fe4fa0a67e062e29a65fe1ef74aa79b
asifzinx <asifzinx@gmail.com>
2019-11-02 23:15:38 +0800

    adding prerequisites install script for maven to run

--- app_code/simple-java-maven-app/jenkins/scripts/pre-install.sh
+++ app_code/simple-java-maven-app/jenkins/scripts/pre-install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 echo 'The following commands install Maven to run Maven-built Java application'
-sudo yum install -y java-1.8.0-openjdk-devel
-java -version
-cd /usr/local/src
-sudo wget http://www-us.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
-sudo tar -xf apache-maven-3.5.4-bin.tar.gz
-sudo mv apache-maven-3.5.4/ apache-maven/ 
-cd /etc/profile.d/
-sudo vim maven.sh
-sudo export M2_HOME=/usr/local/src/apache-maven
-sudo export PATH=${M2_HOME}/bin:${PATH}
-sudo chmod +x maven.sh
-sudo source /etc/profile.d/maven.sh
+# sudo yum install -y java-1.8.0-openjdk-devel
+# java -version
+# cd /usr/local/src
+# sudo wget http://www-us.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
+# sudo tar -xf apache-maven-3.5.4-bin.tar.gz
+# sudo mv apache-maven-3.5.4/ apache-maven/ 
+# cd /etc/profile.d/
+# sudo vim maven.sh
+# sudo export M2_HOME=/usr/local/src/apache-maven
+# sudo export PATH=${M2_HOME}/bin:${PATH}
+# sudo chmod +x maven.sh
+# sudo source /etc/profile.d/maven.sh
 mvn --version
