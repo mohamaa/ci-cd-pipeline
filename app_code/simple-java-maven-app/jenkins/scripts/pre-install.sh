441f7e9ffe188e0705b92984068c158243f5514d
asifzinx <asifzinx@gmail.com>
2019-11-02 22:05:19 +0800

    adding prerequisites install script for maven to run

--- app_code/simple-java-maven-app/jenkins/scripts/pre-install.sh
+++ app_code/simple-java-maven-app/jenkins/scripts/pre-install.sh
@@ -2,7 +2,9 @@
 
 echo 'The following commands install Maven to run Maven-built Java application'
 
-sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-sudo yum install -y apache-maven
+wget http://mirrors.wuchna.com/apachemirror/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
+
+sudo mkdir -p /opt/maven
+sudo tar -xvzf apache-maven-3.6.0-bin.tar.gz -C /opt/maven/ --strip-components=1
+sudo ln -s /opt/maven/bin/mvn /usr/bin/mvn
 mvn --version
