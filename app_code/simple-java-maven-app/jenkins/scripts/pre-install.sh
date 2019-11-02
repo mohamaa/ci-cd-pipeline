0fe1919171ae1188072a04ad4220fee0032aaae1
asifzinx <asifzinx@gmail.com>
2019-11-02 22:11:20 +0800

    adding prerequisites install script for maven to run

--- app_code/simple-java-maven-app/jenkins/scripts/pre-install.sh
+++ app_code/simple-java-maven-app/jenkins/scripts/pre-install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 echo 'The following commands install Maven to run Maven-built Java application'
-
-wget http://mirrors.wuchna.com/apachemirror/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
-
-sudo mkdir -p /opt/maven
-sudo tar -xvzf apache-maven-3.6.0-bin.tar.gz -C /opt/maven/ --strip-components=1
-sudo ln -s /opt/maven/bin/mvn /usr/bin/mvn
+yum install -y java-1.8.0-openjdk-devel
+java -version
+cd /usr/local/src
+wget http://www-us.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
+tar -xf apache-maven-3.5.4-bin.tar.gz
+mv apache-maven-3.5.4/ apache-maven/ 
+cd /etc/profile.d/
+vim maven.sh
+export M2_HOME=/usr/local/src/apache-maven
+export PATH=${M2_HOME}/bin:${PATH}
+chmod +x maven.sh
+source /etc/profile.d/maven.sh
 mvn --version
