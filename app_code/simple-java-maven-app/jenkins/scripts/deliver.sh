9bf656ce52f505bdf627d6d95698a53353f61d4d
asifzinx <asifzinx@gmail.com>
2019-11-02 23:31:35 +0800

    adding prerequisites install script for maven to run

--- app_code/simple-java-maven-app/jenkins/scripts/deliver.sh
+++ app_code/simple-java-maven-app/jenkins/scripts/deliver.sh
@@ -5,6 +5,7 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
+cd app_code/simple-java-maven-app/
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
@@ -23,5 +24,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-cd app_code/simple-java-maven-app/
 java -jar target/${NAME}-${VERSION}.jar
